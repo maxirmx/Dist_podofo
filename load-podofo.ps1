3bc6048cd21838fbf9e3f3275a837c0327034b06
maxirmx <maxim@samsonov.net>
2020-06-22 17:41:52 +0300

    Load podofo & libraries

---
+++ load-podofo.ps1
@@ -0,0 +1,22 @@
+param (
+  [string] $version="0.9.6"
+)
+
+$fname  = "podofo-$version" 
+$fname1 = "$fname.tar"
+$fname2 = "$fname1.gz"
+$uri    =  "https://sourceforge.net/projects/podofo/files/podofo/$version/podofo-$version.tar.gz/download"
+
+# https://adamtheautomator.com/automating-downloads-from-sourceforge/
+Invoke-WebRequest -Uri $uri -OutFile $fname2 -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome
+
+
+& "C:\Program Files\7-Zip\7z" -aoa x $fname2 
+& "C:\Program Files\7-Zip\7z" -aoa x $fname1 
+
+Remove-Item $fname2
+Remove-Item $fname1
+
+Rename-Item -Path $fname -NewName podofo
+
+
---
+++ local/mkall.cmd
@@ -0,0 +1,2 @@
+powershell -ExecutionPolicy Bypass %~dp0\..\load-podofo.ps1  
+powershell -ExecutionPolicy Bypass %~dp0\..\load-libraries.ps1 %1 %2
