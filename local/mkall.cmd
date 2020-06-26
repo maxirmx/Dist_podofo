a38591f220277fd6e23a627aafc9b874a3e64ca5
maxirmx <maxim@samsonov.net>
2020-06-26 17:20:06 +0300

    Zlib, libpng GitHub deploy

--- local/mkall.cmd
+++ local/mkall.cmd
@@ -1,3 +1,3 @@
 powershell -ExecutionPolicy Bypass %~dp0\..\load-podofo.ps1  
-powershell -ExecutionPolicy Bypass %~dp0\..\load-libraries.ps1 zlib %1 %2
-powershell -ExecutionPolicy Bypass %~dp0\..\load-libraries.ps1 libpng %1 %2
+powershell -ExecutionPolicy Bypass %~dp0\..\load-library.ps1 zlib %1 %2
+powershell -ExecutionPolicy Bypass %~dp0\..\load-library.ps1 libpng %1 %2
