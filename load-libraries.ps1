ff98bff27a94c0bd3ec14610c51b38e08a00165e
maxirmx <maxim@samsonov.net>
2020-06-26 17:18:40 +0300

    ZLib, libpng GitHub deploy

--- load-libraries.ps1
+++ load-libraries.ps1
@@ -1,4 +1,5 @@
 param (
+   [parameter(Mandatory=$true)][string]$libname,
 #  platform: either 'X64' or 'x86'
 #            'amd64' is accepted as well and converted to 'X64'
    [parameter(Mandatory=$false)][string]$platform="X64",
@@ -6,44 +7,36 @@ param (
    [parameter(Mandatory=$false)][string]$configuration="debug"
 )
 
-function load {
-  param (
-    [string] $libname,
-    [string] $platform = "X64",
-    [string] $configuration = "debug"
-  )
 
-  $fname = "$libname.7z"
-  $dname = $libname
-  $uri = "http://www.samsonov.net/wp/assets/Dist_$libname/$platform-$configuration/$fname"
-  
-  Write-Host "`nLoading $uri into $dname/$fname `n"
-
-  New-Item -ItemType Directory -Force -Path $dname
-
-  cd $dname
-
-  Invoke-WebRequest -Uri $uri -OutFile $fname
-  & "C:\Program Files\7-Zip\7z" -aoa x $fname
-  Remove-Item $fname
-
-  cd ..
-}
-
- switch($platform) {
+switch($platform) {
    "X64"   { break; } 
    "x86"   { break; } 
    "amd64" { $platform = "X64"; break; } 
    default { "load-libraries.ps1: platform <$platform> was not recognized"; exit (-1);  } 
- }
+}
 
- switch($configuration) {
+switch($configuration) {
    "release"   { break; } 
    "debug"     { break; } 
    default     { "load-libraries.ps1: configuration <$configuration> was not recognized"; exit (-1);  } 
- }
+}
 
 
-load "zlib"  $platform $configuration
-load "libpng" $platform $configuration
+$fname = "$libname.7z"
+$fname_r = "$libname-$platform-$configuration.7z"
+$dname = $libname
+$uri   = "https://github.com/maxirmx/Dist_$libname/releases/latest/download/$fname_r"
+
+Write-Host "`nLoading $uri into $dname/$fname `n"
+
+New-Item -ItemType Directory -Force -Path $dname
+
+cd $dname
+
+Invoke-WebRequest -Uri $uri -OutFile $fname
+& "C:\Program Files\7-Zip\7z" -aoa x $fname
+Remove-Item $fname
+
+cd ..
+
 
--- local/mkall.cmd
+++ local/mkall.cmd
@@ -1,2 +1,3 @@
 powershell -ExecutionPolicy Bypass %~dp0\..\load-podofo.ps1  
-powershell -ExecutionPolicy Bypass %~dp0\..\load-libraries.ps1 %1 %2
+powershell -ExecutionPolicy Bypass %~dp0\..\load-libraries.ps1 zlib %1 %2
+powershell -ExecutionPolicy Bypass %~dp0\..\load-libraries.ps1 libpng %1 %2
