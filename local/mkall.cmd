50e808fe619ee80c5435c56e2a1a7953dcf5989d
maxirmx <maxim@samsonov.net>
2020-08-10 17:10:07 +0300

    EZTools #1

--- local/mkall.cmd
+++
@@ -1,4 +0,0 @@
-powershell -ExecutionPolicy Bypass %~dp0\..\load-podofo.ps1  
-powershell -ExecutionPolicy Bypass %~dp0\..\load-library.ps1 zlib %1 %2
-powershell -ExecutionPolicy Bypass %~dp0\..\load-library.ps1 libpng %1 %2
-powershell -ExecutionPolicy Bypass %~dp0\..\load-library.ps1 freetype2 %1 %2
