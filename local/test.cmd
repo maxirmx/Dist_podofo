9bbb221947b11e241e273505445095192dd2401c
maxirmx <maxim@samsonov.net>
2020-08-11 17:01:58 +0300

    cmake #1

--- local/test.cmd
+++ local/test.cmd
@@ -10,8 +10,18 @@ rem                   (load-library) default is 'debug'
 rem --------------------------------------------------------------------
 @echo on
 
-powershell -ExecutionPolicy Bypass %~dp0\..\load-podofo.ps1 
-powershell -ExecutionPolicy Bypass %~dp0\..\EZTools\load-library.ps1 zlib %1 %2
-powershell -ExecutionPolicy Bypass %~dp0\..\EZTools\load-library.ps1 libpng %1 %2
-powershell -ExecutionPolicy Bypass %~dp0\..\EZTools\load-library.ps1 libjpeg %1 %2
-powershell -ExecutionPolicy Bypass %~dp0\..\EZTools\load-library.ps1 freetype2 %1 %2
+powershell -ExecutionPolicy Bypass %~dp0..\load-podofo.ps1 
+rem powershell -ExecutionPolicy Bypass %~dp0..\EZTools\load-library.ps1 zlib %1 %2
+rem powershell -ExecutionPolicy Bypass %~dp0..\EZTools\load-library.ps1 libpng %1 %2
+rem powershell -ExecutionPolicy Bypass %~dp0..\EZTools\load-library.ps1 libjpeg %1 %2
+rem powershell -ExecutionPolicy Bypass %~dp0..\EZTools\load-library.ps1 freetype2 %1 %2
+
+set FTDIR="..\freetype2"
+set PNGDIR="..\libpng"
+set JPEGDIR="..\libjpeg"
+set ZLIBDIR="..\zlib" 
+
+set
+
+cmake -G "NMake Makefiles" podofo -DCMAKE_INCLUDE_PATH="%FTDIR%\include;%PNGDIR%\include;%JPEGDIR%\include;%ZLIBDIR%\include" -DCMAKE_LIBRARY_PATH="%FTDIR%\lib;%PNGDIR%\lib;%JPEGDIR%\lib;%ZLIBDIR%\lib" -DPODOFO_BUILD_SHARED:BOOL=FALSE -DFREETYPE_LIBRARY_NAMES_DEBUG=freetype239MT_D -DFREETYPE_LIBRARY_NAMES_RELEASE=freetype239MT
+
