cad0e09f9f2c4564ad2f9e2039ad13ae56320c4d
maxirmx <maxim@samsonov.net>
2021-01-13 18:30:39 +0300

    + load libtiff

--- local/build.cmd
+++ local/build.cmd
@@ -16,6 +16,7 @@ IF NOT EXIST podofo    (powershell -ExecutionPolicy Bypass %~dp0..\load-podofo.p
 IF NOT EXIST zlib      (powershell -ExecutionPolicy Bypass %~dp0..\EZTools\load-library.ps1 zlib %1 %2)
 IF NOT EXIST libpng    (powershell -ExecutionPolicy Bypass %~dp0..\EZTools\load-library.ps1 libpng %1 %2)
 IF NOT EXIST libjpeg   (powershell -ExecutionPolicy Bypass %~dp0..\EZTools\load-library.ps1 libjpeg %1 %2)
+IF NOT EXIST libtiff   (powershell -ExecutionPolicy Bypass %~dp0..\EZTools\load-library.ps1 libtiff %1 %2)
 IF NOT EXIST freetype  (powershell -ExecutionPolicy Bypass %~dp0..\EZTools\load-library.ps1 freetype %1 %2)
 
 powershell -ExecutionPolicy Bypass .\build-podofo.ps1 %1 %2 -configure
